0a0555ac155034758f9d10074c29bd5e67b0fdbd
RohanAbrahamJerry <rohanabrahamjerry@gmail.com>
2025-09-01 23:21:47 +0530

    Add files via upload

---
+++ smart stock/src/model/Product.java
@@ -0,0 +1,39 @@
+package model;
+
+public class Product {
+    private int id;
+    private String name;
+    private String description;
+    private double price;
+    private int stock;
+
+    // Constructor
+    public Product(int id, String name, String description, double price, int stock) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+}
