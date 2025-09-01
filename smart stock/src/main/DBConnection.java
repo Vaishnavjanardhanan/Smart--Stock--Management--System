3a88dce43a4a3c41401c0bf00a4c4a56f5b6257b
jestinsource <jestinjayas2065@gmail.com>
2025-09-01 22:34:14 +0530

    Add files via upload

---
+++ smart stock/src/main/DBConnection.java
@@ -0,0 +1,15 @@
+package main;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/smart_stock_db";
+    private static final String USER = "root"; // change if needed
+    private static final String PASSWORD = "Jestin@Mysql"; // change if needed
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
