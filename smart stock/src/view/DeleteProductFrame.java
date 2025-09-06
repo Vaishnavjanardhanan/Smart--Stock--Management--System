0af8a3e4657ff9877675ac2643484b09e0ef72c3
Jestin Jayas <jestinjayas2065@gmail.com>
2025-09-06 22:30:07 +0530

    Update DeleteProductFrame.java

--- smart stock/src/view/DeleteProductFrame.java
+++ smart stock/src/view/DeleteProductFrame.java
@@ -7,15 +7,5 @@ import model.ProductDAO;
 public class DeleteProductFrame extends JFrame {
     public DeleteProductFrame() {
         setTitle("Delete Product");
-        setSize(300, 150);
-        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        setLocationRelativeTo(null);
-
-        JPanel panel = new JPanel(new GridLayout(2, 2, 10, 10));
-        JTextField productIdField = new JTextField();
-        JButton deleteBtn = new JButton("Delete");
-
-        panel.add(new JLabel("Product ID:"));
-        panel.add(productIdField);
-        panel.add(new JLabel(""));
+     
       
