fbcb77ce892eeb6ef76e14438d01e7cada06053f
Jestin Jayas <jestinjayas2065@gmail.com>
2025-09-26 00:20:04 +0530

    added time stamp Customer.java

--- smart stock/src/model/Customer.java
+++ smart stock/src/model/Customer.java
@@ -1,18 +1,28 @@
 package model;
 
+import java.util.Date;
+
 public class Customer {
     private int id;
     private String name;
     private String email;
     private String phone;
     private String address;
+    private Date registrationDate; // Add this field
 
-    public Customer(int id, String name, String email, String phone, String address) {
+    // Updated constructor with registrationDate
+    public Customer(int id, String name, String email, String phone, String address, Date registrationDate) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.phone = phone;
         this.address = address;
+        this.registrationDate = registrationDate;
+    }
+
+    // Existing constructor for backward compatibility
+    public Customer(int id, String name, String email, String phone, String address) {
+        this(id, name, email, phone, address, new Date()); // Default to current date
     }
 
     public int getId() { return id; }
@@ -20,11 +30,13 @@ public class Customer {
     public String getEmail() { return email; }
     public String getPhone() { return phone; }
     public String getAddress() { return address; }
+    public Date getRegistrationDate() { return registrationDate; } // Added method
 
     public void setName(String name) { this.name = name; }
     public void setEmail(String email) { this.email = email; }
     public void setPhone(String phone) { this.phone = phone; }
     public void setAddress(String address) { this.address = address; }
+    public void setRegistrationDate(Date registrationDate) { this.registrationDate = registrationDate; }
 
     @Override
     public String toString() {
@@ -34,6 +46,7 @@ public class Customer {
                 ", email='" + email + '\'' +
                 ", phone='" + phone + '\'' +
                 ", address='" + address + '\'' +
+                ", registrationDate=" + registrationDate +
                 '}';
     }
 }
